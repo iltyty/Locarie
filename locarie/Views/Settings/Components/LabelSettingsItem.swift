f6a3a7e20bd57cfce722e7b2e03fa0849b79f2d7
iltyty <qiuyh0924@163.com>
2024-01-04 10:41:16 +0800

    feat: SocialAccountsPage UI

---
+++ locarie/Views/Settings/Components/LabelSettingsItem.swift
@@ -0,0 +1,25 @@
+//
+//  LabelSettingsItem.swift
+//  locarie
+//
+//  Created by qiuty on 04/01/2024.
+//
+
+import SwiftUI
+
+struct LabelSettingsItem<L: View>: View {
+  let label: L
+  let value: String
+
+  var body: some View {
+    VStack {
+      HStack {
+        label
+        Spacer()
+        Text(value)
+      }
+      Divider()
+    }
+    .padding(.horizontal)
+  }
+}
