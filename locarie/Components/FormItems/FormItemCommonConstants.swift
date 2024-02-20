c9541db46d82959b0143785ee1c75860d5835052
iltyty <qiuyh0924@163.com>
2024-02-20 17:09:24 +0800

    feat: TextEditFormItemWithInlineTitle

---
+++ locarie/Components/FormItems/FormItemCommonConstants.swift
@@ -0,0 +1,12 @@
+//
+//  FormItemCommonConstants.swift
+//  locarie
+//
+//  Created by qiuty on 20/02/2024.
+//
+import SwiftUI
+
+enum FormItemCommonConstants {
+  static let height: CGFloat = 48
+  static let cornerRadius: CGFloat = 25
+}
